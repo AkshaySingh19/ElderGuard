8f3f19fb0961a9c5d3e3d55bb1e896fe28ebede2
AkshaySingh19 <akshaysingh63810@gmail.com>
2025-03-23 15:06:19 +0530

    Update registration.js

--- registration.js
+++ registration.js
@@ -8,3 +8,22 @@ const firebaseConfig = {
     appId: "1:201757709021:web:576a8f9aead45cb16c4134",
     measurementId: "G-Z4E77KYKTL"
   };
+// this is js code for register button
+const button1=document.getElementById('btn');
+button1.addEventListener("click", openDashboard);
+
+function openDashboard(){
+    window.location.href="dashboard.html";
+}
+
+// document.getElementById('loginbtn').addEventListener("click", function(event) {
+//     event.preventDefault(); // Prevent the default link behavior
+//     window.location.href = "login.htm"; // Redirect to login page in the same tab
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("loginbtn").addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent default anchor link behavior
+        window.location.href = "login.htm"; // Redirect to login page in the same tab
+    });
+});
